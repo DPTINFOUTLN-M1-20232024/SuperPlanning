30ec8ff7edf6abff44bf1d7edec8b4eef4d3c179
Alexandre L <alexandre-lerousseau@etud.univ-tln.fr>
2023-12-11 17:58:08 +0100

    Add SessionGUI, to create rectangle that represent sessions on the WeekEDT

--- src/main/java/fr/wedidit/superplanning/superplanning/utils/gui/SessionGUI.java
+++ src/main/java/fr/wedidit/superplanning/superplanning/utils/gui/SessionGUI.java
@@ -7,6 +7,8 @@ import javafx.scene.shape.Rectangle;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Calendar;
+
 @Getter
 @Setter
 public class SessionGUI {
@@ -17,10 +19,15 @@ public class SessionGUI {
     private static final int YSHIFT = 48;
 
     private Session session;
+    private Calendar calendarDay; //Date of day
+    private final int intDay;
+    private final float floatHour;
+
     private Rectangle emplacementRectangle;
     private Label moduleNameLabel;
     private Label instructorNameLabel;
     private Label roomNameLabel;
+    private static final String labelColor = "#FFFFFF";
 
     public static SessionGUI of(Session session) {
         SessionGUI sessionGUI = new SessionGUI(session);
@@ -29,8 +36,12 @@ public class SessionGUI {
         return sessionGUI;
     }
     private SessionGUI(Session session) {
+        this.calendarDay = Calendar.getInstance();
+        this.calendarDay.setTimeInMillis(session.getBegin().getTime());
+        this.intDay = this.calendarDay.get(Calendar.DAY_OF_WEEK);
+        this.floatHour = this.sizeRectangle();
         this.session = session;
-        this.emplacementRectangle = new Rectangle(SessionGUI.XSHIFT, SessionGUI.YSHIFT);
+        this.emplacementRectangle = new Rectangle(SessionGUI.XSHIFT, SessionGUI.YSHIFT * this.sizeRectangle());
         this.moduleNameLabel = new Label(session.getModule().getName());
         this.instructorNameLabel = new Label(session.getInstructor().getLastname());
         this.roomNameLabel = new Label(session.getRoom().getName());
@@ -40,18 +51,36 @@ public class SessionGUI {
         /*
         Prepare widgets from the scene that have been initialised.
          */
+
         //todo : Rajouter la couleur changeable par rapport à la session
-        this.emplacementRectangle.setFill(Paint.valueOf("#444444"));
+        this.emplacementRectangle.setFill(Paint.valueOf(ColorOfSession.CM));
+        this.moduleNameLabel.setTextFill(Paint.valueOf(SessionGUI.labelColor));
+        this.instructorNameLabel.setTextFill(Paint.valueOf(SessionGUI.labelColor));
+        this.roomNameLabel.setTextFill(Paint.valueOf(SessionGUI.labelColor));
     }
 
     private void moveWidgets() {
         /*
         Move widgets from the scene to their right place.
          */
-
-        this.emplacementRectangle.setX(SessionGUI.XINITPOS);
+        float xLabelShift = (float)SessionGUI.XSHIFT/2;
+        float yLabelShift = (float)SessionGUI.YSHIFT/6;
+        int xInitShift = SessionGUI.XINITPOS * this.intDay;
+        this.emplacementRectangle.setX(xInitShift);
         this.emplacementRectangle.setY(SessionGUI.YINITPOS);
-        this.moduleNameLabel.setTranslateX(SessionGUI.XINITPOS + (SessionGUI.XSHIFT>>1));
-        this.moduleNameLabel.setTranslateY(SessionGUI.YINITPOS + ((SessionGUI.YSHIFT/3)));
+        this.moduleNameLabel.setTranslateX(xInitShift + ((int)xLabelShift));
+        this.moduleNameLabel.setTranslateY(SessionGUI.YINITPOS + ((int)yLabelShift));
+        this.instructorNameLabel.setTranslateX(xInitShift + ((int)xLabelShift));
+        this.instructorNameLabel.setTranslateY(SessionGUI.YINITPOS + ((int)yLabelShift)*3);
+        this.roomNameLabel.setTranslateX(xInitShift + ((int)xLabelShift));
+        this.roomNameLabel.setTranslateY(SessionGUI.YINITPOS + ((int)yLabelShift)*5);
+    }
+
+    private float sizeRectangle() {
+        /*
+        Return the number of pixels that are necessary to make the
+        rectangle.
+         */
+        return (this.session.getFinish().getTime() - this.session.getBegin().getTime())/((float)3600000); //3600000 millisecondes = 3600 secondes
     }
 }
