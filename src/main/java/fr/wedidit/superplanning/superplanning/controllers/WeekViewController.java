fbd53c3145e53afc0e14cb32a85b703ca50c8dd8
Saladdine MW <82659633+Saladdine-77@users.noreply.github.com>
2023-12-14 22:04:48 +0100

    getting sessions for a week and display them by day-functional part

--- src/main/java/fr/wedidit/superplanning/superplanning/controllers/WeekViewController.java
+++ src/main/java/fr/wedidit/superplanning/superplanning/controllers/WeekViewController.java
@@ -1,12 +1,28 @@
 package fr.wedidit.superplanning.superplanning.controllers;
 
+import fr.wedidit.superplanning.superplanning.database.dao.daolist.humans.StudentDAO;
+import fr.wedidit.superplanning.superplanning.database.dao.daolist.others.ModuleDAO;
+import fr.wedidit.superplanning.superplanning.database.dao.daolist.others.SessionDAO;
+import fr.wedidit.superplanning.superplanning.database.exceptions.DataAccessException;
+import fr.wedidit.superplanning.superplanning.database.exceptions.IdentifiableNotFoundException;
+import fr.wedidit.superplanning.superplanning.identifiables.humans.Student;
+import fr.wedidit.superplanning.superplanning.identifiables.others.Grade;
+import fr.wedidit.superplanning.superplanning.identifiables.others.Module;
+import fr.wedidit.superplanning.superplanning.identifiables.others.Session;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Pagination;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.DayOfWeek;
+import java.util.*;
 
 public class WeekViewController {
+    /*DAO utilisation*/
+    private SessionDAO sessionDAO;
+    private StudentDAO studentDAO;
+    private ModuleDAO moduleDAO;
 
     /* MODEL PART */
 
@@ -22,7 +38,7 @@ public class WeekViewController {
     @FXML
     private Label promotionUser;
 
-    /* WeekView Planning informations */
+    /* WeekView Planning information */
     @FXML
     private Pagination weekPagination;
 
@@ -47,11 +63,43 @@ public class WeekViewController {
         SceneSwitcher.switchToScene(event, "DailyView.fxml");
     }
 
-    private void showWeekSessions() {
-        /*
-        Shows every sessions of the week for a selected student.
-         */
-        // todo : Implanter ce code avec des objets de la classe SessionGUI
+
+    private void showWeekSessions(long studentId, Timestamp beginPossible, Timestamp finishPossible) throws DataAccessException {
+        Optional<Student> st = studentDAO.find(studentId);
+        if (st.isEmpty()) {
+            throw new IdentifiableNotFoundException(studentId);
+        }
+        firstNameUser.setText(st.get().getFirstname());
+        Grade gr = st.get().getGrade();
+        Set<Module> modules = moduleDAO.getModulesFromGrade(gr);
+        Set<Session> sessionsWeek = new HashSet<>();//=sessionDAO.getSessionsFromModuleBetween()
+        for (var mod : modules) {
+            sessionsWeek.addAll(sessionDAO.getSessionsFromModuleBetween(mod, beginPossible, finishPossible));
+
+        }
+        displaySessionsByDay(sessionsWeek);
+
 
     }
-}
+
+    private void displaySessionsByDay(Set<Session> sessionsWeek) {
+
+        Map<DayOfWeek, Long> sessionsCountByDay;
+        sessionsCountByDay = new HashMap<>();
+        for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+            sessionsCountByDay.put(dayOfWeek, 0L);
+        }
+
+        for (Session session : sessionsWeek) {
+            DayOfWeek dayOfWeek = session.getBegin().toLocalDateTime().getDayOfWeek();
+            sessionsCountByDay.put(dayOfWeek, sessionsCountByDay.get(dayOfWeek) + 1);
+        }
+
+
+        mondayDate.setText("Monday: " + sessionsCountByDay.get(DayOfWeek.MONDAY));
+        tuesdayDate.setText("Tuesday: " + sessionsCountByDay.get(DayOfWeek.TUESDAY));
+        wednesdayDate.setText("Wednesday: " + sessionsCountByDay.get(DayOfWeek.WEDNESDAY));
+        thursdayDate.setText("Thursday: " + sessionsCountByDay.get(DayOfWeek.THURSDAY));
+        fridayDate.setText("Friday: " + sessionsCountByDay.get(DayOfWeek.FRIDAY));
+    }
+    }
