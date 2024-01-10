ab80fe8b2e56439958806b70d82236a61c258b91
Alexandre L <alexandre-lerousseau@etud.univ-tln.fr>
2024-01-10 17:08:38 +0100

    Change StartH2 into H2Server to open and close the server

--- src/main/java/fr/wedidit/superplanning/superplanning/database/H2Server.java
+++ src/main/java/fr/wedidit/superplanning/superplanning/database/H2Server.java
@@ -3,6 +3,7 @@ package fr.wedidit.superplanning.superplanning.database;
 import fr.wedidit.superplanning.superplanning.database.datasources.DBCPDataSource;
 import fr.wedidit.superplanning.superplanning.properties.PropertyLoader;
 import fr.wedidit.superplanning.superplanning.utils.others.FileUtils;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.h2.tools.RunScript;
 import org.h2.tools.Server;
@@ -11,17 +12,16 @@ import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-/**
- * Class used to start the H2 server.
- */
+@Getter
 @Slf4j
-public class StartH2 {
-
+public class H2Server {
     private static final String DDB_PORT = "9092";
+    private static Server server;
 
-    public static void main(String[] args) {
+    private H2Server() {}
+    public static void open() {
         try {
-            Server server = Server.createTcpServer("-tcpPort", DDB_PORT, "-tcpAllowOthers", "-pgAllowOthers", "-ifNotExists", "-baseDir", "/tmp/db").start();
+            server = Server.createTcpServer("-tcpPort", DDB_PORT, "-tcpAllowOthers", "-pgAllowOthers", "-ifNotExists", "-baseDir", "/tmp/db").start();
             log.info(server.getStatus());
         } catch (SQLException sqlException) {
             log.info("Server start error. " + sqlException.getLocalizedMessage());
@@ -42,5 +42,9 @@ public class StartH2 {
         } catch (SQLException sqlException) {
             log.error("Unable to execute feedDB.sql file:%n%s".formatted(sqlException.getLocalizedMessage()));
         }
+    }
+
+    public static void close() {
+        server.stop();
     }
 }
